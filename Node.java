2e577989797906ac4c7a15f5052752a57e3e41e9
DylanRafTorres <dylandtgi@gmail.com>
2024-11-26 13:03:04 -0800

    Node updates
    noders

--- Node.java
+++ Node.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 
 public class Node {
-    int data;
+    int data; //primary key
+    String firstName, lastName, phoneNumber, address, city, state, zipCode, email; //user data  
     Node left;
     Node right;
 
-    public Node(int data){
+    public Node(int data, String firstName, String lastName, String phoneNumber, 
+    String address, String city, String state, String zipCode, String email){ 
+        
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.email = email;
         this.data = data;
     }
 }
